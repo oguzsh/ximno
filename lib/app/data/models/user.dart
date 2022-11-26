8249e24d6d51bdbbce2edf268c673ad410a2748d
Oguzhan Ince <oguzhan824@gmail.com>
2022-11-27 00:06:23 +0300

    add: user api and user model class

---
+++ lib/app/data/models/user.dart
@@ -0,0 +1,4 @@
+class User {
+  static mapFromJson(Map<String, dynamic> json) {}
+  static fromJson(user) {}
+}
---
+++ lib/app/data/providers/api/user_api.dart
@@ -0,0 +1,28 @@
+import 'package:get/get.dart';
+
+import '../../models/user.dart';
+import 'base/base_api.dart';
+
+class UserAPI extends BaseAPI {
+  UserAPI() : super('users');
+
+  // Future<List<User>> getAll() async {
+  //   // TODO: implement all
+  // }
+
+  // Future<User> getUser(String uuid) async {
+  //   // TODO: implement get
+  // }
+
+  // Future<User> createUser(User user) async {
+  //   // TODO: implement create
+  // }
+
+  // Future<User> updateUser(User user) async {
+  //   // TODO: implement update
+  // }
+
+  // Future<User> deleteUser(String uuid) async {
+  //   // TODO: implement delete
+  // }
+}
