27817a9b7b53c8c555b3905a97792dcc325b1bfe
Oguzhan Ince <oguzhan824@gmail.com>
2022-11-27 00:06:45 +0300

    add: user service class

---
+++ lib/app/data/services/user_service.dart
@@ -0,0 +1,24 @@
+import 'package:ximno/app/data/providers/api/base/base_api.dart';
+import 'base_service.dart';
+
+class UserService implements BaseService {
+  @override
+  void all() {
+    // TODO: implement all
+  }
+
+  @override
+  void create(data) {
+    // TODO: implement create
+  }
+
+  @override
+  void get(String uuid) {
+    // TODO: implement get
+  }
+
+  @override
+  void remove(String uuid) {
+    // TODO: implement remove
+  }
+}
